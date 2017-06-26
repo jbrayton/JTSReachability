0976950ce881387314a45eafefc4e516f18481c4
John Brayton <jbrayton@goldenhillsoftware.com>
2017-06-26 15:28:52 -0400

    Carthage-ready Xcode project.

---
+++ JTSReachability/JTSReachability.h
@@ -0,0 +1,19 @@
+//
+//  JTSReachability.h
+//  JTSReachability
+//
+//  Created by John Brayton on 6/26/17.
+//  Copyright © 2017 John Brayton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JTSReachability.
+FOUNDATION_EXPORT double JTSReachabilityVersionNumber;
+
+//! Project version string for JTSReachability.
+FOUNDATION_EXPORT const unsigned char JTSReachabilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JTSReachability/PublicHeader.h>
+
+
