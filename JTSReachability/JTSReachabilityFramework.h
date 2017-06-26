aff01ffe52d4e68679fafcd0119a9ebb837d7042
John Brayton <jbrayton@goldenhillsoftware.com>
2017-06-26 18:28:57 -0400

    rearrange headers

---
+++ JTSReachability/JTSReachabilityFramework.h
@@ -0,0 +1,16 @@
+//
+//  JTSReachabilityFramework.h
+//  JTSReachability
+//
+//  Created by John Brayton on 6/26/17.
+//  Copyright © 2017 John Brayton. All rights reserved.
+//
+
+#ifndef JTSReachabilityFramework_h
+#define JTSReachabilityFramework_h
+
+
+#import <JTSReachability/JTSReachability.h>
+#import <JTSReachability/JTSReachabilityResponder.h>
+
+#endif /* JTSReachabilityFramework_h */
